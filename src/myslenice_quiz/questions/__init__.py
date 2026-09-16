4b456483c0442ab92d45d1c80ce3a605cf2cd686
kri1oof <19chris900@gmail.com>
2026-09-16 16:04:55 +0200

    Include unusual context questions in generator

--- src/myslenice_quiz/questions/__init__.py
+++ src/myslenice_quiz/questions/__init__.py
@@ -1,10 +1,12 @@
 from .generators import generate_all as _generate_core
 from .social_questions import generate_social_context_questions
+from .special_context_questions import generate_special_context_questions
 
 
 def generate_all(conn, min_confidence: float = 0.80):
     questions = list(_generate_core(conn, min_confidence))
     questions.extend(generate_social_context_questions(conn, min_confidence))
+    questions.extend(generate_special_context_questions(conn, min_confidence))
     return list({q.id: q for q in questions}.values())
 
 
