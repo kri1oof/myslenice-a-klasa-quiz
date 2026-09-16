fd4bbec2751b6796ca4ef19d2d515911bfa6a619
kri1oof <19chris900@gmail.com>
2026-09-16 14:58:31 +0200

    Include verified social context in question generation

--- src/myslenice_quiz/questions/__init__.py
+++ src/myslenice_quiz/questions/__init__.py
@@ -1,3 +1,11 @@
-from .generators import generate_all
+from .generators import generate_all as _generate_core
+from .social_questions import generate_social_context_questions
+
+
+def generate_all(conn, min_confidence: float = 0.80):
+    questions = list(_generate_core(conn, min_confidence))
+    questions.extend(generate_social_context_questions(conn, min_confidence))
+    return list({q.id: q for q in questions}.values())
+
 
 __all__ = ["generate_all"]
