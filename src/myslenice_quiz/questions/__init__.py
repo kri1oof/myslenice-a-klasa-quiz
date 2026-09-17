44231c73333cb5ae2662b4efa33e3fb99676d741
kri1oof <19chris900@gmail.com>
2026-09-17 12:54:34 +0200

    Include ŁNP-derived questions

--- src/myslenice_quiz/questions/__init__.py
+++ src/myslenice_quiz/questions/__init__.py
@@ -1,4 +1,5 @@
 from .generators import generate_all as _generate_core
+from .lnp_questions import generate_lnp_questions
 from .social_questions import generate_social_context_questions
 from .social_story_questions import generate_social_story_questions
 from .special_context_questions import generate_special_context_questions
@@ -6,6 +7,7 @@ from .special_context_questions import generate_special_context_questions
 
 def generate_all(conn, min_confidence: float = 0.80):
     questions = list(_generate_core(conn, min_confidence))
+    questions.extend(generate_lnp_questions(conn, min_confidence))
     questions.extend(generate_social_context_questions(conn, min_confidence))
     questions.extend(generate_social_story_questions(conn, min_confidence))
     questions.extend(generate_special_context_questions(conn, min_confidence))
